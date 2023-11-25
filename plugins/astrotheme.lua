333226024d807c6ba0ac079f243d9b6263dbaf0d
Skop-22 <skopflores@gmail.com>
2023-11-24 22:24:54 -0600

    Corrección de colores Cambios a ser confirmados: 	modificados:     plugins/astrotheme.lua

--- plugins/astrotheme.lua
+++ plugins/astrotheme.lua
@@ -6,14 +6,14 @@ return {
       dark = "astrodark",
     },
     style = {
-      transparent = true,         -- Bool value, toggles transparency.
+      transparent = true,           -- Bool value, toggles transparency.
       inactive = false,             -- Bool value, toggles inactive window color.
       float = false,                -- Bool value, toggles floating windows background colors.
       neotree = false,              -- Bool value, toggles neo-trees background color.
-      border = true,               -- Bool value, toggles borders.
-      title_invert = true,         -- Bool value, swaps text and background colors.
-      italic_comments = true,      -- Bool value, toggles italic comments.
-      simple_syntax_colors = true, -- Bool value, simplifies the amounts of colors used for syntax highlighting.
+      border = true,                -- Bool value, toggles borders.
+      title_invert = false,         -- Bool value, swaps text and background colors.
+      italic_comments = true,       -- Bool value, toggles italic comments.
+      simple_syntax_colors = true,  -- Bool value, simplifies the amounts of colors used for syntax highlighting.
     },
     termguicolors = true, -- Bool 
     terminal_color = true, -- Bool 
