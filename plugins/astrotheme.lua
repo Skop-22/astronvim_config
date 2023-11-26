f980d14f61f3aa2d3e0571a8bd55a81554fcc6ae
Skop-22 <skopflores@gmail.com>
2023-11-26 10:39:53 -0600

    config/lsp

--- plugins/astrotheme.lua
+++ plugins/astrotheme.lua
@@ -20,7 +20,7 @@ return {
     palettes = {
       global = {             -- Globally accessible palettes, theme palettes take priority.
         my_grey = "#b1faaf",
-        my_color = "#719996"
+        my_color = "#aaaaaa"
       },    
     },
     highlights = {
