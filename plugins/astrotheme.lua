20dce91e786444980a11e2c10aa0911788430962
Skop-22 <skopflores@gmail.com>
2023-11-27 21:03:19 -0600

    Cambios a ser confirmados: 	modificados:     plugins/astrotheme.lua

--- plugins/astrotheme.lua
+++ plugins/astrotheme.lua
@@ -20,7 +20,7 @@ return {
     palettes = {
       global = {             -- Globally accessible palettes, theme palettes take priority.
         my_grey = "#b1faaf",
-        my_color = "#335555"
+        my_color = "#888888"
       },    
     },
     highlights = {
