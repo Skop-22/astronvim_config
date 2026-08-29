ac792aa535052fc263164fba4ebf8287e88aa8be
skop <skopflores@gmail.com>
2026-08-29 17:19:47 -0600

    ajuses de laravel plugin
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     lua/plugins/laravel.lua

--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -1,35 +1,30 @@
 return {
   "adalessa/laravel.nvim",
   dependencies = {
-    "tpope/vim-dotenv",
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-neotest/nvim-nio",
-    -- "ravitemer/mcphub.nvim", -- optional
   },
-  cmd = { "Laravel" },
+  ft = { "php", "blade" },
+  event = { "BufEnter composer.json" },
   keys = {
-    { "<leader>Ll", function() Laravel.pickers.laravel() end,              desc = "Laravel: Open Laravel Picker" },
-    { "<c-g>",      function() Laravel.commands.run("view:finder") end,    desc = "Laravel: Open View Finder" },
-    { "<leader>La", function() Laravel.pickers.artisan() end,              desc = "Laravel: Open Artisan Picker" },
-    { "<leader>Lt", function() Laravel.commands.run("actions") end,        desc = "Laravel: Open Actions Picker" },
-    { "<leader>Lr", function() Laravel.pickers.routes() end,               desc = "Laravel: Open Routes Picker" },
-    { "<leader>Lh", function() Laravel.run("artisan docs") end,            desc = "Laravel: Open Documentation" },
-    { "<leader>Lm", function() Laravel.pickers.make() end,                 desc = "Laravel: Open Make Picker" },
-    { "<leader>Lc", function() Laravel.pickers.commands() end,             desc = "Laravel: Open Commands Picker" },
-    { "<leader>Lo", function() Laravel.pickers.resources() end,            desc = "Laravel: Open Resources Picker" },
-    { "<leader>Lp", function() Laravel.commands.run("command_center") end, desc = "Laravel: Open Command Center" },
-    { "<F4>", function() Laravel.commands.run("tinker:open") end, desc = "Laravel: Open command tinker open" },
+    { "<leader>Ll", function() Laravel.pickers.laravel() end,                   desc = "Laravel: Picker" },
+    { "<leader>La", function() Laravel.pickers.artisan() end,                   desc = "Laravel: Artisan Picker" },
+    { "<leader>Lm", function() Laravel.pickers.make() end,                      desc = "Laravel: Make Picker" },
+    { "<leader>Lc", function() Laravel.pickers.commands() end,                  desc = "Laravel: Custom Commands Picker" },
+    { "<leader>Lo", function() Laravel.pickers.resources() end,                 desc = "Laravel: Resources Picker" },
+    { "<leader>Lh", function() Laravel.run("artisan docs") end,                 desc = "Laravel: Documentation" },
+    { "<leader>Lt", function() Laravel.commands.run("actions") end,             desc = "Laravel: Code Actions" },
+    { "<leader>Lu", function() Laravel.commands.run("hub") end,                 desc = "Laravel: Artisan Hub" },
+    { "<leader>Lp", function() Laravel.commands.run("command_center") end,      desc = "Laravel: Command Center" },
+    { "<F9>",       function() Laravel.commands.run("view:finder") end,         desc = "Laravel: View Finder" },
+    { "<F10>",      function() Laravel.pickers.routes() end,                    desc = "Laravel: Routes Picker" },
+    { "<F4>",       function() Laravel.commands.run("tinker:open") end,         desc = "Laravel: Open command tinker open" },
   },
-  event = { "VeryLazy" },
   opts = {
     features = {
-      override = {
-        enable = true,
-      },
       pickers = {
-        enable = true,
-        provider = "snacks", -- "snacks | telescope | fzf-lua | ui-select"
+        provider = "snacks", -- telescope | fzf-lua | snacks | ui.select
       },
     },
   },
